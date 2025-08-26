681e663d8934932debc2df9cf5bbad35350186d4
MrOleg <mrdrozdilla@gmail.com>
2025-08-26 17:25:04 +0200

    Initial commit

---
+++ src/main/java/com/home/project/dragonrockets/DragonrocketsApplication.java
@@ -0,0 +1,8 @@
+package com.home.project.dragonrockets;
+
+public class DragonrocketsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("SpaceX Dragon Rockets repository");
+	}
+}
